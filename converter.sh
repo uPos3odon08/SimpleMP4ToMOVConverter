10e84881b2592d34d1d59db7e94c192855ebfb11
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2023-06-04 16:16:37 +0200

    Update converter.sh

--- converter.sh
+++ converter.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "No file specified. Usage: ./converter [path to mp4 file]"
+    echo "No file specified. Usage: ./converter [--mp4tomov/--movtomp4] [path to file]"
     exit 1
 fi
 
-file="$1"
-if [[ $file == *.mp4 ]]; then
-    filename="${file%.*}"
-    output_filename="${filename}_mjpeg.mov"
+flag="$1"
+file="$2"
 
-    ffmpeg -i "$file" -vcodec mjpeg -q:v 2 -acodec pcm_s16be -q:a 0 -f mov "$output_filename"
-
-    echo "Conversion complete: $output_filename"
+if [ "$flag" == "--mp4tomov" ]; then
+    if [[ $file == *.mp4 ]]; then
+        filename="${file%.*}"
+        output_filename="${filename}.mov"
+        ffmpeg -i "$file" -c:v prores -profile:v 3 -c:a pcm_s16le "$output_filename"
+        echo "Conversion complete: $output_filename"
+    else
+        echo "Invalid file format. Only MP4 files are supported."
+    fi
+elif [ "$flag" == "--movtomp4" ]; then
+    if [[ $file == *.mov ]]; then
+        filename="${file%.*}"
+        output_filename="${filename}.mp4"
+        ffmpeg -i "$file" -c:v libx264 -preset medium -crf 23 -c:a aac -b:a 128k "$output_filename"
+        echo "Conversion complete: $output_filename"
+    else
+        echo "Invalid file format. Only MOV files are supported."
+    fi
 else
-    echo "Invalid file format. Only MP4 files are supported."
+    echo "Invalid flag. Usage: ./converter [--mp4tomov/--movtomp4] [path to file]"
 fi
