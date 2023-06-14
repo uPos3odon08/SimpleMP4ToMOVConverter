410320bed675eb3bc426c5fbaba52baaaaabb09a
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2023-06-14 13:21:06 +0200

    Update converter.sh
    added nvidia nvenc support

--- converter.sh
+++ converter.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "No file specified. Usage: ./converter [--mp4tomov/--movtomp4] [path to file]"
+if [ $# -lt 2 ]; then
+    echo "No file specified. Usage: ./converter [--enable-nvenc] [--mp4tomov/--movtomp4] [path to file]"
     exit 1
 fi
 
-flag="$1"
-file="$2"
+flag=""
+file=""
+
+if [[ "$1" == "--enable-nvenc" ]]; then
+    flag="$2"
+    file="$3"
+else
+    flag="$1"
+    file="$2"
+fi
 
 if [ "$flag" == "--mp4tomov" ]; then
     if [[ $file == *.mp4 ]]; then
         filename="${file%.*}"
         output_filename="${filename}.mov"
-        ffmpeg -i "$file" -c:v prores -profile:v 3 -c:a pcm_s16le "$output_filename"
+        if [[ "$1" == "--enable-nvenc" ]]; then
+            ffmpeg -i "$file" -c:v h264_nvenc -profile:v high -preset medium -c:a pcm_s16le "$output_filename"
+        else
+            ffmpeg -i "$file" -c:v prores -profile:v 3 -c:a pcm_s16le "$output_filename"
+        fi
         echo "Conversion complete: $output_filename"
     else
         echo "Invalid file format. Only MP4 files are supported."
@@ -21,11 +33,15 @@ elif [ "$flag" == "--movtomp4" ]; then
     if [[ $file == *.mov ]]; then
         filename="${file%.*}"
         output_filename="${filename}.mp4"
-        ffmpeg -i "$file" -c:v libx264 -preset medium -crf 23 -c:a aac -b:a 128k "$output_filename"
+        if [[ "$1" == "--enable-nvenc" ]]; then
+            ffmpeg -i "$file" -c:v h264_nvenc -preset medium -crf 23 -c:a aac -b:a 128k "$output_filename"
+        else
+            ffmpeg -i "$file" -c:v libx264 -preset medium -crf 23 -c:a aac -b:a 128k "$output_filename"
+        fi
         echo "Conversion complete: $output_filename"
     else
         echo "Invalid file format. Only MOV files are supported."
     fi
 else
-    echo "Invalid flag. Usage: ./converter [--mp4tomov/--movtomp4] [path to file]"
+    echo "Invalid flag. Usage: ./converter [--enable-nvenc] [--mp4tomov/--movtomp4] [path to file]"
 fi
